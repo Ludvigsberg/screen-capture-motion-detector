33a31058849d74837526d15e3dbdaed945cdf4f6
Ludvigsberg <133618513+Ludvigsberg@users.noreply.github.com>
2023-05-18 20:18:12 +0300

    Create sensor.py
    Using dxcam and cv2 for detecting motion on the screen. Output is resized for performance reasons but can be upscaled for better visualizing if need be. On my ultrawide I'm getting around 45 FPS which is a lot better than 16 FPS that I was getting with using mss sct.

---
+++ sensor.py
@@ -0,0 +1,39 @@
+import dxcam
+import cv2
+from time import time
+
+camera = dxcam.create()
+camera.start(target_fps=120)
+
+object_detector = cv2.createBackgroundSubtractorMOG2(
+    history=100, varThreshold=3, detectShadows=False
+)
+
+scale_percentage = 50
+loop_time = time()
+
+while True:
+    frame = camera.get_latest_frame()
+
+    cv2.imshow(
+        'Movement Detector',
+        object_detector.apply(
+            cv2.resize(
+                frame,
+                (
+                    int((frame.shape[1] * scale_percentage) * 0.01),
+                    int((frame.shape[0] * scale_percentage) * 0.01),
+                ),
+                interpolation=cv2.INTER_AREA,
+            ),
+        ),
+    )
+
+    print('FPS {}'.format(1 / (time() - loop_time)))
+    loop_time = time()
+
+    if (cv2.waitKey(1) & 0xFF) == ord('q'):
+        cv2.destroyAllWindows()
+        break
+
+camera.stop()
